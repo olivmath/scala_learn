762bedba2bae753823f2cfbe938928fea5fae92d
Lucas <olivmath@protonmail.com>
2021-02-09 21:59:21 -0300

    Variaveis Atualizado

--- 01-var_val.scala
+++ 01-var_val.scala
@@ -9,6 +9,39 @@ object VarVal extends App {
 	val nome: String = "Lucas"
 
 	// Mutaveis
-	var idade: Int = 23
+	var idade: uint = 23000
 	var estadoCivil: String = "casado"
+
+	/*
+		Tipos de Variaveis:
+	*/
+
+	// Boolean
+	val p: Boolean = true
+
+	// Byte
+	val b: Byte = 101
+
+	// Char
+	val letra: Char = 'a'
+
+	// String
+	val seqLetra: String = "abcdefg"
+
+	// Short
+	val numeroPequeno: Short = 123
+
+	// Int
+	val numero: Int = 62000
+
+	// Long
+	val numeroGrande: Long = 12000300L
+
+	// Float
+	val fracao: Float = 3.1415F
+
+	// Double
+	val fracaoPrecisa: Double = 3.1415639574D
+
+
 }
--- 02-collections.scala
+++ 02-collections.scala
@@ -5,14 +5,18 @@ object Collections extends App {
 		Collections são os estruturas de dados
 	*/
 
-	// List 
-	- index, immutable
+	def sortAlg(arr: Array[Int]): Array[Int] = {
+		for(i <- 0 to arr.length)
+			for(j <- 0 to arr.length)
+				if(arr(i) > arr(j)) {
+					println(arr(i))
+				} else {
+					println(arr(j))
+				}
+		arr
+	}
 
-
-	// Array
-	- index, immutable
-
-	// Map
-	- index, immutable
+	var arr = Array(9,8,5,6,4,11,2,3133,987,125,45,62,33)
+	println(sortAlg(arr))
 
 }
