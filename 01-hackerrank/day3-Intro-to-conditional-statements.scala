4ead96da672bfbdaf0a0b3c9d5bcefbb5b11780e
Andre <andrean86@gmail.com>
2021-02-24 23:13:32 -0300

    helpmeLucas

---
+++ 01-hackerrank/day3-Intro-to-conditional-statements.scala
@@ -0,0 +1,34 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val N = stdin.readLine().trim.toInt
+        val Weird = N.readLine()
+        val Not_Weird = N.readLine()
+        
+        
+    
+    }
+        
+    def Weird: String, Not_Weird: String = {
+    if(Weird == 3) "impar"
+    else if(Not_Weird == 24) "par"
+    } 
+        
+    println(N(Weird, Not_Weird))
+}
+
+/*Lucas me ajuda, já tentei de tudo kkkkkkkk*/
